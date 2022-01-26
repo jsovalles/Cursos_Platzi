5fffb4d75831498636d151ad1bd35504b392821c
Sebastian Ovalle <juanovalle@westhillglobal.com>
2022-01-26 17:21:57 -0500

    adding new classes

---
+++ src/keywords.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//defer
+	// Se ejecuta de ultimas cuando todo el flujo halla sido recorrido
+	defer fmt.Println("hola")
+	fmt.Println("Mundo")
+
+// Continue y break
+	for i := 0; i < 10; i++{
+		fmt.Println(i)
+		if i == 4 {
+			fmt.Println("Es 4")
+			continue
+		}
+
+		// Break
+		if i == 4 {
+			fmt.Println("Break")
+			break
+		}
+}
+
+}
